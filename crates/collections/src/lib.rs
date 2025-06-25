3de5b209e2fd0822b744e42314cffc7bc2acd2ff
Bálits Péter <balitspm@gmail.com>
2025-06-25 09:40:42 +0200

    remove custom linked list + cleanup

--- crates/collections/src/lib.rs
+++ crates/collections/src/lib.rs
@@ -1,7 +1,35 @@
 #![feature(linked_list_cursors)]
+mod dict;
+mod hash_table;
 
-pub mod hashmap;
-pub(crate) mod linked_list;
-mod macros;
 
-pub use linked_list::*;
+pub use dict::Dict;
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub struct Entry {
+    key: String,
+    value: String,
+}
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum PartialRehashError {
+    #[error("Unexpected size of {table_name} name, got: {size_got}, expected: {size_expected}")]
+    InvalidTableSize {
+        table_name: String,
+        size_got: usize,
+        size_expected: usize,
+    },
+    #[error("Rehash index invalid got: {rehash_idx}, valid range: -1..{table_size}")]
+    InvalidRehashIndex {
+        rehash_idx: isize,
+        table_size: usize,
+    },
+    #[error("Couldnt rehash table while load load factor is not high enough")]
+    IncorrectLoadFactor {
+        rehash_idx: isize,
+        load_factor: usize,
+    }
+}
+
